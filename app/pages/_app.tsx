bcb2030a85adb6620e67a67e6bb989c1a7fadb2f
Grant Findlay <grant@tate.it>
2022-01-29 12:52:14 +0000

    init app

---
+++ app/pages/_app.tsx
@@ -0,0 +1,32 @@
+import * as anchor from "@project-serum/anchor";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { AppProps } from "next/app";
+import { useMemo } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  // You can also provide a custom RPC endpoint
+  const endpoint = anchor.web3.clusterApiUrl("devnet"); // "http://localhost:8899";
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const queryClient = useMemo(() => new QueryClient(), []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <Component {...pageProps} />
+        </WalletProvider>
+      </ConnectionProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
